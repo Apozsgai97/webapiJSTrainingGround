87060950d4be4c980e7347b201dfb048ec477ad9
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-09-18 08:53:55 +0100

    Console.log the request url and method.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-
+    console.log(`The URL for the request was '${req.url}'`);
+    console.log(`The Method for the request was '${req.method}'`);
     const content = fs.readFileSync('./static/index.html', 'utf-8');
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
@@ -13,5 +14,5 @@ const hostname = 'localhost';
 const port = 3000;
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
