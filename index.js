a67ee704b99cb2d88f7124d856ee1a32aed582de
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-09-20 09:20:17 +0100

    handling error with getFileContentOr404 function

--- index.js
+++ index.js
@@ -11,18 +11,28 @@ const fileNameOfUrl = url => {
     return fileName;
 }
 
+const getFileContentOr404 = (fileName) => {
+    if(!fs.existsSync(`./static/${fileName}`)){
+        fileName = '404.html';
+    }
+
+    return fs.readFileSync(`./static/${fileName}`, 'utf-8');
+}
+
 const server = http.createServer((req, res) => {
     console.log(`The URL for the request was '${req.url}'`);
     console.log(`The Method for the request was '${req.method}'`);
 
     const fileName = fileNameOfUrl(req.url);
+    
     if(fileName === 'favicon.ico') {
         res.statusCode = 404;
         res.end('');
         return;
     }
 
-    const content = fs.readFileSync(`./static/${fileName}`, 'utf-8');
+    const content = getFileContentOr404(fileName);
+    
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end(content);
