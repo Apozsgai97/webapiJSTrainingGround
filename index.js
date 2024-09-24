5efa3b10f9149ba24e5551f0672a4c72c0ac2103
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-09-24 09:02:53 +0100

    Create webserver with express

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const fileNameOfUrl = url => { 
-    let fileName = '';
-    if(url.split('/')[1] === ''){
-        fileName = 'index.html'
-    } else {
-        fileName = url.split('/')[1]
-    }
-    return fileName;
-}
-
-const getFileContentOr404 = (fileName) => {
-    if(!fs.existsSync(`./static/${fileName}`)){
-        fileName = '404.html';
-    }
-
-    return fs.readFileSync(`./static/${fileName}`, 'utf-8');
-}
-
-const server = http.createServer((req, res) => {
-    console.log(`The URL for the request was '${req.url}'`);
-    console.log(`The Method for the request was '${req.method}'`);
-
-    const fileName = fileNameOfUrl(req.url);
-    
-    if(fileName === 'favicon.ico') {
-        res.statusCode = 404;
-        res.end('');
-        return;
-    }
-
-    const content = getFileContentOr404(fileName);
-    
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(content);
+app.get('/', (req, res) => {
+    res.send('Hello fellow developer!');
 });
 
-const hostname = 'localhost';
-const port = 3000;
+const port = 3000
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
