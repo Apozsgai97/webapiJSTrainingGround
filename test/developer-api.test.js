30fe7ec57083cb4e39011088a3de68fdcf02e23d
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-09-26 17:09:58 +0200

    Fix test to work

--- test/developer-api.test.js
+++ test/developer-api.test.js
@@ -1,9 +1,14 @@
 const assert = require('assert');
 const request = require('supertest');
+const api = require('../api');
 
 
 describe('developer API should have endpoints to', () => {
-    it('get all developers', () => {
-        assert.strictEqual(false, true);
+    it('get all developers', (done) => {
+        request(api)
+            .get('/api/developers')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     })
 })
